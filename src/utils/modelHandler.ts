7527bbc7231de0f08c4dfa79de6a67b612b20c0d
djohnson <mr.demarcus.johnson@gmail.com>
2024-07-15 07:32:40 +0200

    fix: load percentage should increase

--- src/utils/modelHandler.ts
+++ src/utils/modelHandler.ts
@@ -3,7 +3,7 @@ import { LLMDescriptor, LLMLoadModelOpts, LMStudioClient } from '@lmstudio/sdk'
 import _ from 'lodash'
 
 //  NOTE: if you're using a different port or url, change it here (needs to be a web socket)
-const client = new LMStudioClient({baseUrl: 'ws://127.0.0.1:1234'})
+const client = new LMStudioClient({ baseUrl: 'ws://127.0.0.1:1234' })
 
 class ModelHandler {
     getLoaded = async () => {
@@ -41,6 +41,8 @@ class ModelHandler {
                 if (loadPercentage !== lastLoadPercent) {
                     handleLoadProgress(lastLoadPercent)
                 }
+
+                lastLoadPercent = loadPercentage
             }
         })
 
